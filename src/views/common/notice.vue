ee33d62d5ee9fa45fa9788b9d2395f629c79dbe3
wsinghai <455493143@qq.com>
2018-02-27 17:33:00 +0800

    dd

--- src/views/common/notice.vue
+++ src/views/common/notice.vue
@@ -52,108 +52,62 @@
 </template>
 
 <script>
-  export default {
-    name: 'app1',
-    data: function () {
-      return {
-        ids: [4],
-        options: {
-          label: 'label',
-          checkbox: true,
-          checkedOpen: false,
-          folderBold: true,
-          idsWithParent: true,
-          depthOpen: 2,
-          openIcon: 'fa fa-angle-right',
-          closeIcon: 'fa fa-angle-down',
-          halfCheckedIcon: 'fa fa-minus-square-o fa-fw text-primary',
-          checkedIcon: 'fa fa-check-square-o fa-fw text-danger',
-          uncheckedIcon: 'fa fa-square-o fa-fw'
-        },
-        message: [],
-        treeData: [{
-          label: '根目录[1]',
-          id: 1,
-          children: [
-            {
-              label: '一级节点[4]',
-              id: 4,
-              children: [
-                {
-                  label: '二级节点[5]',
-                  id: 5,
-                  children: [
-                    {label: '三级节点[6]', id: 6},
-                    {label: '三级节点[8]', id: 8},
-                    {label: '三级节点[30]', id: 30},
-                    {
-                      label: '三级节点[31]',
-                      id: 31,
-                      children: [
-                        {label: '四级节点[36]', id: 36},
-                        {label: '四级节点[38]', id: 38},
-                        {label: '四级节点[39]', id: 39},
-                        {label: '四级节点[48]', id: 48}
-                      ]
-                    }
-                  ]
-                },
-                {label: '二级节点[9]', id: 9},
-                {label: '二级节点[10]', id: 10},
-                {
-                  label: '二级节点[11]',
-                  id: 11,
-                  children: [
-                    {label: '三级节点[12]', id: 12},
-                    {label: '三级节点[13]', id: 13},
-                    {
-                      label: '三级节点[14]',
-                      id: 14,
-                      children: [
-                        {label: '四级节点[15]', id: 15},
-                        {label: '四级节点[16]', id: 16},
-                        {label: '四级节点[17]', id: 17},
-                        {label: '四级节点[18]', id: 18},
-                        {
-                          label: '四级节点[19]',
-                          id: 19,
-                          children: [
-                            {label: '五级节点[20]', id: 20},
-                            {label: '五级节点[21]', id: 21},
-                            {label: '五级节点[22]', id: 22},
-                            {label: '五级节点[23]', id: 23},
-                            {label: '五级节点[24]', id: 24}
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            },
-            {label: '一级节点[2]', id: 2},
-            {
-              label: '一级节点[59]',
-              id: 59,
-              children: [
-                {label: '二级节点[60]', id: 60},
-                {label: '二级节点[61]', id: 61},
-                {label: '二级节点[62]', id: 62},
-                {label: '二级节点[63]', id: 63},
-                {label: '二级节点[64]', id: 64}
-              ]
-            },
-            {label: '一级节点[3]', id: 3}
-          ]
-        }]
-      }
-    },
+import axios from 'axios'
+export default {
+  name: 'app1',
+  data: function () {
+    return {
+      ids: [4],
+      options: {
+        label: 'label',
+        checkbox: true,
+        checkedOpen: false,
+        folderBold: true,
+        idsWithParent: true,
+        depthOpen: 2,
+        openIcon: 'fa fa-angle-right',
+        closeIcon: 'fa fa-angle-down',
+        halfCheckedIcon: 'fa fa-minus-square-o fa-fw text-primary',
+        checkedIcon: 'fa fa-check-square-o fa-fw text-danger',
+        uncheckedIcon: 'fa fa-square-o fa-fw'
+      },
+      sj: '',
+      message: [],
+      treeData: ''
+    }
+  },
+  created:function() {
+    var _this = this
+    var userid = localStorage.getItem("userid")
+    axios.get('/apis/Department/GetTreeListJsonMobile?userId=fdf0dbec-82d6-442c-b0e4-be8dcfcafe0c', {}).then((response) => {
+      console.log('获取组织请求成功')
+      var _this = this
+      _this.treeData = _this.getJsonTree(response.data.rows, '0')
+      console.log(_this.sj )
+    }).catch((response) => {
+      console.log('获取组织请求失败')
+    })
+  },
 
-    methods: {
-      handle (item) {
-        this.message.push(`节点 ${JSON.stringify(item)} 作为事件参数传递`)
+  methods: {
+    handle (item) {
+      this.message.push(`节点 ${JSON.stringify(item)} 作为事件参数传递`)
+    },
+    getJsonTree: function(data, pId) {
+      var itemArr=[];
+      for(var i=0;i<data.length;i++){ 
+        var node=data[i];
+        //data.splice(i, 1)
+        if(node.pId==pId){ 
+          var newNode={children:this.getJsonTree(data,node.id),id:node.id,label:node.name,};
+          itemArr.push(newNode);              
+        } 
       }
+      return itemArr;
     }
   }
+}
+
 </script>
+
 
